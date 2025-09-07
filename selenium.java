72ce5352bf3ca20453e4dd9f830ef9dec02b3bb7
Tanmay Khedekar <khedekartanmay50@gmail.com>
2025-09-07 15:37:48 +0530

    Delete selenium.java

--- selenium.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.By;
-public class selenium {
-    public static void main(String[] args) {
-        System.setProperty("websriver.chrome.driver",)
-        
-    }
-
-    
-}
